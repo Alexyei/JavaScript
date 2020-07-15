1125d718ac1d20a0ffe7d274f92a5fcd606f44ba
Alexyei <41749439+Alexyei@users.noreply.github.com>
2020-07-15 16:01:55 +0300

    js

--- Обучение/alert.js
+++ Обучение/alert.js
@@ -1,14 +1,36 @@
-let value = "Сюрприз!";
-
-function f() {
-    let value = "ближайшее значение";
-
-    function g() {
-        debugger; // в консоли: напишите alert(value); Сюрприз!
+"use strict";
+function sum(a) {
+    sum.value = a;
+    function fsum(a){
+        sum.value+=a;
+        return fsum;
     }
+    fsum[Symbol.toPrimitive]=()=>sum.value;
 
-    return g;
+    return fsum;
 }
 
-let g = f();
-g();
+console.log(sum(1)(2)==3)
+console.log(sum(1)(2)(3) == 6);
+console.log(sum(5)(-1)(2) == 6);
+console.log(sum(6)(-1)(-2)(-3) == 0);
+console.log(sum(0)(1)(2)(3)(4)(5) == 15);
+console.log(sum.value);
+
+function sum1(a) {
+    let value = a;
+    function fsum(a){
+        value+=a;
+        return fsum;
+    }
+    fsum[Symbol.toPrimitive]=()=>value;
+
+    return fsum;
+}
+
+console.log(sum1(1)(2)==3);
+console.log(sum1(1)(2)(3) == 6);
+console.log(sum1(5)(-1)(2) == 6);
+console.log(sum1(6)(-1)(-2)(-3) == 0);
+console.log(sum1(0)(1)(2)(3)(4)(5) == 15);
+console.log(sum1.value);
