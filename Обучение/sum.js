6f7e1e899fdd1d72f24684c0796d04b0bf4d6dbc
Alexyei <41749439+Alexyei@users.noreply.github.com>
2020-07-15 16:03:13 +0300

    js

---
+++ Обучение/sum.js
@@ -0,0 +1,36 @@
+"use strict";
+function sum(a) {
+    sum.value = a;
+    function fsum(a){
+        sum.value+=a;
+        return fsum;
+    }
+    fsum[Symbol.toPrimitive]=()=>sum.value;
+
+    return fsum;
+}
+
+console.log(sum(1)(2)==3)
+console.log(sum(1)(2)(3) == 6);
+console.log(sum(5)(-1)(2) == 6);
+console.log(sum(6)(-1)(-2)(-3) == 0);
+console.log(sum(0)(1)(2)(3)(4)(5) == 15);
+console.log(sum.value);
+
+function sum1(a) {
+    let value = a;
+    function fsum(a){
+        value+=a;
+        return fsum;
+    }
+    fsum[Symbol.toPrimitive]=()=>value;
+
+    return fsum;
+}
+
+console.log(sum1(1)(2)==3);
+console.log(sum1(1)(2)(3) == 6);
+console.log(sum1(5)(-1)(2) == 6);
+console.log(sum1(6)(-1)(-2)(-3) == 0);
+console.log(sum1(0)(1)(2)(3)(4)(5) == 15);
+console.log(sum1.value);
